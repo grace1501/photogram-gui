b988a0d43ad18ed3cfc75765cbfe848938902787
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-27 05:59:13 +0000

    photos index route done

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,11 @@
+class PhotosController < ApplicationController
+  def index
+    @list_of_photos = Photo.all.order(:created_at => :desc)
+
+    render( :template => "photo_templates/index")
+  end
+
+  def show
+    render( :templare => "photo_templates/show")
+  end
+end
